258d8de9e1eff575ba1e04b2694aaff1a113f8b7
Adam Tumminaro <adam@spaceshipb.com>
2017-05-04 15:04:09 -0500

    Added timelion extras

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ assets.push({ name: `x-pack-${version}.zip`, url: `http://artifacts.elastic.co/d
 assets.push({ name: `filebeat-${version}-windows-x86_64.zip`, url: `https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${version}-windows-x86_64.zip`});
 assets.push({ name: `packetbeat-${version}-windows-x86_64.zip`, url: `https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-${version}-windows-x86_64.zip`});
 assets.push({ name: `winlogbeat-${version}-windows-x86_64.zip`, url: `https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-${version}-windows-x86_64.zip`});
-assets.push({ name: `metricbeat-${version}-windows-x86_64.zip`, url: `https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${version}-windows-x86_64.zip`})
-assets.push({ name: `heartbeat-${version}-windows-x86_64.zip`, url: `https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-${version}-windows-x86_64.zip`})
+assets.push({ name: `metricbeat-${version}-windows-x86_64.zip`, url: `https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${version}-windows-x86_64.zip`});
+assets.push({ name: `heartbeat-${version}-windows-x86_64.zip`, url: `https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-${version}-windows-x86_64.zip`});
+assets.push({ name: `timelion-extras-master.zip`, url: `https://github.com/rashidkpc/timelion-extras/archive/master.zip`})
 assets.forEach((asset) => {
   download(asset.url, `${dir}/${asset.name}`);
 })
